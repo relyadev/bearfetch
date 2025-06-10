9914c49115683173b6ce83a22423035dbeae76be
relyadev <relyadev@gmail.com>
2025-06-10 12:30:34 +0300

    add art.py

--- bearfetch.py
+++ bearfetch.py
@@ -2,18 +2,11 @@
 
 from colorama import Fore, init, Style
 from core import hostname, kernel, user, dist, memory, arch
+from core.art import BEAR
 
 init(autoreset=True)
 
-art = """
-   ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄
-   █▒▒░░░░░░░░░▒▒█   
-    █░░█░░░░░█░░█    
- ▄▄  █░░░▀█▀░░░█  ▄▄ 
-█░░█ ▀▄░░░░░░░▄▀ █░░█
-"""
-
-art_lines = [line for line in art.split('\n') if line.strip()]
+art_lines = [line for line in BEAR.split('\n') if line.strip()]
 
 info = [
         f"{Fore.YELLOW}{user.get()}{Style.RESET_ALL}@{Fore.YELLOW}{hostname.get()}{Style.RESET_ALL}",
@@ -28,7 +21,7 @@ art_width = max(len(line) for line in art_lines)
 for i, line in enumerate(art_lines):
     print(f"{Fore.YELLOW}{line}", end="")
     if i < len(info):
-        print(" " * (art_width - len(line) + 2) + info[i])
+        print(" " * (art_width - len(line) + 1) + info[i])
     else:
         print()
 
